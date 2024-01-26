2d7a3782493fa4bfe762064242731eeaf888dd5a
khinlei1998 <khinleiyatihtun98@gmail.com>
2024-01-26 15:10:00 +0630

    loading page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import LoadingPage from './src/components/pages/LoadingPage/LoadingPage';
+import BackgroundImg from './assets/svg/loading_background.svg'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <BackgroundImg/>
+     {/* <LoadingPage/> */}
       <StatusBar style="auto" />
     </View>
   );
