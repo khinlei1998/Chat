a9b7f480f48a799315c3f9e660b9a15692b0f6ff
khinlei1998 <khinleiyatihtun98@gmail.com>
2024-02-15 09:14:38 +0630

    add logo

--- App.tsx
+++ App.tsx
@@ -2,21 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoadingPage from './src/components/pages/LoadingPage/LoadingPage';
 import BackgroundImg from './assets/svg/loading_background.svg'
+import Logo from './assets/svg/logo_1.svg'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <BackgroundImg/>
-     {/* <LoadingPage/> */}
-      <StatusBar style="auto" />
+    <View style={styles.root}>
+      <View style={{ width: '100%', }}>
+        <BackgroundImg />
+
+      </View>
+      <View style={styles.contentViewStyle}>
+        <Logo />
+        <Text style={{ color: 'black' }}>What apps</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  root: {
+    position: 'relative',
+    width: '100%',
+  },
+  contentViewStyle: {
+    position: 'absolute',
+    // display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+    height: '100%'
   },
+
 });
